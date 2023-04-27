6a62fcc59b68dc3d3250eb4b3be2ac9df530cc63
Ashleyg5 <ashleygiraldo5@hotmail.com>
2023-04-27 11:06:35 -0400

    fixed bug in select and delete note

--- routes/html.js
+++ routes/html.js
@@ -15,7 +15,7 @@ noteshtml.post('/', (req, res) => {
       const newNote = {
         title,
         text,
-        note_id: uuid(),
+        id: uuid(),
       };
   
       readAndAppend(newNote, './db/db.json');
